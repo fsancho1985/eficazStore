70c2cdf147581e1d2249659f9c413940e7a53212
fsancho <frsancho1985@gmail.com>
2023-05-11 11:16:20 -0300

    Adicionando novas features e responsividade

--- js/index.js
+++ js/index.js
@@ -1,27 +1,35 @@
 const urlBase = 'https://fakestoreapi.com';
 const textContainer = document.getElementById('text-container');
 
+// API Request
 async function getApiData() {
     try {
         const response = await fetch(`${urlBase}/products/categories`)
         const data = await response.json()
         data.map((el) => {
-            const div = document.createElement('div')
-            const title = document.createElement('h3')
-            const img = document.createElement('img')
-            title.innerText = el;
-            img.setAttribute('src', `./assets/${el}.jpg`)
-            img.setAttribute('alt', `${el}`)
-            img.setAttribute('href', `${urlBase}/products/category/${el}`)
-            div.appendChild(title)
-            div.appendChild(img)
-            div.classList.add('categories-container')
-            textContainer.appendChild(div)
+            createCards(el)
         })
     } catch (error) {
         alert(error)
     }
 
+}
+
+// Function to create Cards
+function createCards(el) {
+    const div = document.createElement('div')
+    const title = document.createElement('h3')
+    const img = document.createElement('img')
+    const link = document.createElement('a')
+    title.innerText = el;
+    link.appendChild(img)
+    link.setAttribute('href', `/catalogo/${el}.html`)
+    img.setAttribute('src', `./assets/${el}.jpg`)
+    img.setAttribute('alt', `${el}`)
+    div.appendChild(title)
+    div.appendChild(link)
+    div.classList.add('categories-container')
+    textContainer.appendChild(div)
 }
 
 getApiData();
